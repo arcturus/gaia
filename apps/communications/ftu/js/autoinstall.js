069b1fc40f542ca532d58d0ec59395caa3283b89
Francisco Jordano <arcturus@ardeenelinfierno.com>
2013-07-02 15:05:45 +0100

    Registering the push channel into remote service

--- apps/communications/ftu/js/autoinstall.js
+++ apps/communications/ftu/js/autoinstall.js
@@ -1,11 +1,32 @@
 var AutoInstall = function AutoInstall() {
 
+  var AUTOINSTALL_ENDPOINT = 'http://autoinstall.eu01.aws.af.cm/';
+
   var settings = window.navigator.mozSettings;
 
   var _register = function register(email, endpoint, callback) {
-    if (callback) {
-      callback();
-    }
+    // Post the info to the server
+    var data = new FormData();
+    data.append('email', email);
+    data.append('client', endpoint);
+
+    var url = AUTOINSTALL_ENDPOINT + '/api/v1/register';
+    var xhr = new XMLHttpRequest({mozSystem: true});
+
+    console.log('Registering against url ' + url);
+
+    xhr.onload = function onLoad(evt) {
+      if (xhr.status === 200 || xhr.status === 0) {
+        callback(null);
+      } else {
+        callback(xhr.status);
+      }
+    };
+    xhr.onerror = function onError(err) {
+      callback(err);
+    };
+    xhr.open('POST', url, true);
+    xhr.send(data);
   };
 
   var configure = function configure(email) {
@@ -14,13 +35,16 @@ var AutoInstall = function AutoInstall() {
     }
 
     if (!navigator.push) {
+      console.log('We dont have push api');
       return;
     }
 
     var req = navigator.push.register();
     req.onsuccess = function onSuccess(evt) {
       var endpoint = req.result;
+      console.log('Register and got endpoint ' + endpoint);
       _register(email, endpoint, function onRegister(error) {
+        console.log('Got a response from regisitration ' + error);
         if (error) {
           console.error(error);
           return;
