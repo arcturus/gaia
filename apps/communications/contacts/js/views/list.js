1a94aec16eed625b13c711a5d57a0a8d6f314689
ashay.neema <ashay.neema@lge.com>
2014-02-07 14:00:24 +0530

    Bug 956219 - Implementation of Multiple Contact Delete

--- apps/communications/contacts/js/views/list.js
+++ apps/communications/contacts/js/views/list.js
@@ -1426,6 +1426,7 @@ contacts.List = (function() {
     ones are selected.
   */
   var selectAction = function selectAction(action) {
+    updateSelectCount(0);
     if (action == null) {
       exitSelectMode();
       return;
@@ -1507,6 +1508,7 @@ contacts.List = (function() {
     selectActionButton.disabled = currentlySelected == 0;
     selectAll.disabled = selectAllDisabled;
     deselectAll.disabled = deselectAllDisabled;
+    updateSelectCount(currentlySelected);
   };
 
   /*
@@ -1810,6 +1812,9 @@ contacts.List = (function() {
         contacts.List.refresh(node.dataset.uuid);
       }
     }
+  };
+  function updateSelectCount(count) {
+    Contacts.updateSelectCountTitle(count);
   };
 
   return {
