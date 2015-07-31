7b978bfdd4b66aba64b6d55f7a0882d5ff1df8c3
Francisco Jordano <fjordano@mozilla.com>
2015-07-31 15:28:56 +0100

    Adding more logging and trying to modify the cached files on a app

--- dev_apps/sw_testing/precached/sw.js
+++ dev_apps/sw_testing/precached/sw.js
@@ -1,21 +1,55 @@
+/* global caches,  performance, fetch, Request, Response */
+'use strict';
 var cachePromise = caches.open('offline');
 
-addEventListener('activate', function(evt) {
-  var operations = [];
-  operations.push(self.clients.claim());
-  operations.push(cachePromise.then(cache => {
-    return cache.addAll(['/img/test.png', '/precached/install-sw.js',
-    '/precached/precached.html']);
+addEventListener('install', function(evt) {
+  console.log('Installing');
+  evt.waitUntil(cachePromise.then( cache => {
+    // var requests = [];
+    // ['/img/test.png', '/precached/install-sw.js',
+    // '/precached/precached.html'].forEach(url => {
+    //   requests.push(new Request(url));
+    // });
+    // return cache.addAll(requests);
+    var files = {
+      '/img/test.png': 'image/png',
+      '/precached/precached.html': 'text/html',
+      '/precached/install-sw.js': 'application/javascript'
+    };
+    var requests = [];
+    var urls = Object.keys(files);
+    urls.forEach(url => {
+      requests.push(fetch(url));
+    });
+
+    return Promise.all(requests).then(responses => {
+      var cacheCalls = [];
+      responses.forEach((response, index) => {
+        var url = urls[index];
+        var request = new Request(url);
+        var newResponse = new Response(response.body,
+           {'content-type': files[url]});
+        cacheCalls.push(cache.put(request, newResponse));
+      });
+      return Promise.all(cacheCalls);
+    });
   }));
-  evt.waitUntil(Promise.all(operations));
+});
+
+addEventListener('activate', function(evt) {
+  console.log('Activating');
+  evt.waitUntil(self.clients.claim());
 });
 
 addEventListener('fetch', function(evt) {
+  console.log('Doing fetch for ' + evt.request.url);
   performance.clearMarks();
   performance.mark('fetch_start');
   evt.respondWith(cachePromise.then(cache => {
+    console.log('I have a cache ', cache);
     performance.mark('cache_start');
     cache.match(evt.request).then(response => {
+      console.log('Doing the matching with response ', response.clone());
       performance.measure('time_to_cache', 'cache_start');
       if (response) {
         performance.measure('time_to_fetch', 'fetch_start');
@@ -34,7 +68,7 @@ addEventListener('message', function(evt) {
   measures.forEach(function(measure) {
     console.log(JSON.stringify(measure));
   });
-  clients.matchAll().then(clients => {
+  self.clients.matchAll().then(clients => {
     clients[0].postMessage(JSON.stringify(measures));
   });
 });
